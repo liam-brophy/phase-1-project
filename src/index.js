fd616c5fb451378ee8631839fcae77d9ac19069d
KerissaGit <kerissa525@gmail.com>
2025-01-17 17:09:35 -0500

    Updates and additions to favorite button and displays as well as some HTML updates for Favorites

--- src/index.js
+++ src/index.js
@@ -15,26 +15,64 @@ async function fetchData() {
 console.log(fetchData())
 
 
+
 // Creating a Favorites list
 let favorites = [];
 
 function createFavorites() = {
   //Get class name to retrieve art pieces to save for list
-  const favoriteList = document.getElementsByClassName("");
+  const favoriteList = document.getElementsById("");
   favoriteList.innerText = ''
 
-  favorites.forEach(favorites => {
-    const listOfFavorites = document.createElement('li')
-    listFavArt.textContext = `Art Piece: ${favoriteList.id}`
-    favoriteList.appendChild(listFavArt)
+  favorites.forEach(favoritesList => {
+    const listArt = document.createElement('li')
+    listArt.textContext = `Art Piece: ${favoritesList}`
+    favoriteList.appendChild(listArt)
   })
 }
 
 //Click Event Listener for Favorites Button
 const favButton = document.getElementById('fav-button')
 
+//!!Will need adjusting depending on names of code for image grabs!!
 function favClick() {
+  const imgId = e.target.getAttribute('data-img-id')
+  const imgElement = document.getElementById(imgId)
+  const imageUrl = imgElement.src
   alert("Favorite Button Clicked")
 }
 
 favButton.addEventListener('click', favClick)
+
+document.querySelectorAll('fav-button').forEach(button => {
+  button.addEventListener('click', async (event) => {
+    const imgId = event.target.getAttribute('data-img-id')
+    const imgElement = document.getElementById(imgId)
+    const imageUrl = imgElement.src; 
+
+    if (!favorites.includes(imageUrl)) {
+      try {
+        favorites.push(imageUrl)
+        await saveFavorite(imageUrl)
+        updateFavoritesDisplay()
+      } catch (error) {
+        alert(error)
+      }
+    } else {
+      alert('This picture is already in your favorites!');
+    }
+  });
+});
+
+
+// const updateFavoritesDisplay = () => {
+//   favoriteImagesContainer.innerHTML = ''; // Clear the current favorites list
+
+//   // Loop through the favorites array and display each image
+//   favorites.forEach(imageUrl => {
+//     const imgElement = document.createElement('img');
+//     imgElement.src = imageUrl;
+//     imgElement.alt = 'Favorite Picture';
+//     favoriteImagesContainer.appendChild(imgElement);
+//   });
+// };
