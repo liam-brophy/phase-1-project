f23aabebbd327abd0e6337ad5164891f667c8219
KerissaGit <kerissa525@gmail.com>
2025-01-17 15:56:50 -0500

    Added function to create a favorites list and a Click event button. Base level .

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 
+
+//API function Data
 async function fetchData() {
   const apiUrl = "https://api.artic.edu/api/v1/artworks";
   const response = await fetch(apiUrl, {
@@ -11,3 +13,28 @@ async function fetchData() {
 
 
 console.log(fetchData())
+
+
+// Creating a Favorites list
+let favorites = [];
+
+function createFavorites() = {
+  //Get class name to retrieve art pieces to save for list
+  const favoriteList = document.getElementsByClassName("");
+  favoriteList.innerText = ''
+
+  favorites.forEach(favorites => {
+    const listOfFavorites = document.createElement('li')
+    listFavArt.textContext = `Art Piece: ${favoriteList.id}`
+    favoriteList.appendChild(listFavArt)
+  })
+}
+
+//Click Event Listener for Favorites Button
+const favButton = document.getElementById('fav-button')
+
+function favClick() {
+  alert("Favorite Button Clicked")
+}
+
+favButton.addEventListener('click', favClick)
