8973619f1b84d6951b6e83f5671f9c8bd2202b68
Liam Brophy <liambrophy.design@gmail.com>
2025-01-23 09:04:44 -0500

    updates theme toggle js

--- src/index.js
+++ src/index.js
@@ -300,6 +300,30 @@ function displayArtwork(artwork) {
 }
 
 
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme) {
+    body.classList.add(savedTheme);
+    themeToggle.textContent = savedTheme === "dark-mode" ? "Switch to Light Mode" : "Switch to Dark Mode";
+    themeToggle.className = savedTheme;
+}
+
+// Toggle theme on button click
+themeToggle.addEventListener("click", () => {
+    if (body.classList.contains("dark-mode")) {
+        body.classList.replace("dark-mode", "light-mode");
+        themeToggle.textContent = "Switch to Dark Mode";
+        themeToggle.className = "light-mode";
+        localStorage.setItem("theme", "light-mode");
+    } else {
+        body.classList.replace("light-mode", "dark-mode");
+        themeToggle.textContent = "Switch to Light Mode";
+        themeToggle.className = "dark-mode";
+        localStorage.setItem("theme", "dark-mode");
+    }
+});
+
+
+
 //!EVENT LISTENERS
 document.getElementById("conjure-button").addEventListener("click", handleFilterChange);
 document.getElementById("movement-select").addEventListener("change", handleFilterChange);
